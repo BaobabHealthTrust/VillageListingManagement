97a66ad5aed25d52e87b4d54d631ee543b5e98e9
jmziya <jacobmziya@gmail.com>
2019-01-11 11:37:18 +0200

    Forcing push.

--- app/models/user_tracker.rb
+++ app/models/user_tracker.rb
@@ -1,14 +1,15 @@
 require 'couchrest_model'
 
 class UserTracker < CouchRest::Model::Base
-	
+
 	use_database :user_tracker
-	
+
 	property :person_tracker, String
 	property :username, String
-	
+	property :user_role, String
+
 	timestamps!
-	
+
 	design do
 		view :by__id,
 		     :map => "function(doc) {
@@ -16,16 +17,16 @@ class UserTracker < CouchRest::Model::Base
                         emit(doc['_id'], 1);
                     }
              }"
-		
+
 		view :by_person_tracker,
 		     :map => "function(doc) {
 					if((doc['type'] == 'UserTracker')) {
 						emit(doc.person_tracker, 1);
 					}
 			 }"
-		
+
 		view :by_updated_at
-		
+
 		view :by_created_at
 	end
 
